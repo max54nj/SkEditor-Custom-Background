1c3bfc80d29d1f008e2533b820b7de24049d5967
Max54nj <contact@max54nj.de>
2025-10-26 22:21:15 +0100

    feat: make sidebars respect opacity setting

--- SkEditor-Custom-Background/Utilities/Background.cs
+++ SkEditor-Custom-Background/Utilities/Background.cs
@@ -18,16 +18,16 @@ public class Background
             
             if (!settings.KeepEditorBackground)
             {
-                // StyleOverrides.ApplyFile("EditorBackground");
-
                 var opacity = (int)Settings.Settings.Instance.BackgroundOpacity;
                 var hexValue = (int)(opacity / 100.0 * 255);
                 var opacityHex = hexValue.ToString("X2");
                 var backgroundBrush = new ImmutableSolidColorBrush(Color.Parse($"#{opacityHex}ffffff"));
                 var transparentBrush = new ImmutableSolidColorBrush(Color.Parse("#00ffffff"));
+                
                 StyleOverrides.Apply("EditorBackgroundColor", transparentBrush);
                 StyleOverrides.Apply("TabViewSelectedItemBorderBrush", backgroundBrush);
                 StyleOverrides.Apply("TabViewItemHeaderBackgroundSelected", backgroundBrush);
+                StyleOverrides.Apply("SkEditorBorderBackground", backgroundBrush);
             }
         });
     }
